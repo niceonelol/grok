3d2c55a63b70e2be7f9a98acfdc07d1b5dff4eb2
Arya Narang <aryanarang2005@gmail.com>
2026-03-29 11:36:14 +0100

    feat: completed data analysis on multiplication operation

--- fyp/analysis.py
+++ fyp/analysis.py
@@ -3,8 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def plot_accuracies_phdim_against_epochs(input_file, output_file):
-    df = pd.read_csv(input_file).sort_values('epoch')
+ROOT = "fyp/data"
+
+def plot_accuracies_phdim_against_epochs(input_dir):
+    """
+        input_dir: Directory name of the operation (i.e. the child directory in fyp/data)
+                   Examples: 'addition', 'multiplication' etc.
+
+        This function plots training acc, validation acc and Persistent Homology Dimension
+        against epochs. CSV file must follow format of that outputted by scripts/train.py
+    """
+
+    input_parent_dir = os.path.join(ROOT, input_dir)
+    df = pd.read_csv(os.path.join(input_parent_dir, input_dir + ".csv")).sort_values('epoch')
 
     df_train = df.dropna(subset=['train_accuracy'])
     df_val = df.dropna(subset=['val_accuracy'])
@@ -25,24 +36,72 @@ def plot_accuracies_phdim_against_epochs(input_file, output_file):
     ax2 = ax1.twinx()
     ax2.plot(df_ph['epoch'], df_ph['phdim_0'], color='red', linewidth=0.8, label='Persistent Homology Dimension')
     
-    ax2.set_yscale('log')
-    ax2.set_ylabel('Persistent Homology Dimension (Log Scale)')
+    #ax2.set_yscale('log')
+    ax2.set_ylim(bottom=0)
+    ax2.set_ylabel('Persistent Homology Dimension ')
     ax2.tick_params(axis='y')
 
-    plt.title('addition operation')
+    plt.title(f"{input_dir} operation")
 
     lines1, labels1 = ax1.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
     ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left', fontsize='x-small')
 
     plt.tight_layout()
-    plt.savefig(output_file)
+    plt.savefig(os.path.join(input_parent_dir, input_dir + "_acc.png"))
     plt.show()
 
-def concat_csvs(csv_list, output_name):
+def plot_losses_phdim_against_epochs(input_dir): 
+    input_parent_dir = os.path.join(ROOT, input_dir)
+    df = pd.read_csv(os.path.join(input_parent_dir, input_dir + ".csv")).sort_values('epoch')
+
+    df_train = df.dropna(subset=['train_loss'])
+    df_val = df.dropna(subset=['val_loss'])
+    df_ph = df[df['phdim_0'] >= 0].dropna(subset=['phdim_0'])
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    ax1.set_facecolor('#e6f3ff') 
+
+    ax1.plot(df_train['epoch'], df_train['train_loss'], color='blue', linewidth=0.8, label='Train Loss')
+    ax1.plot(df_val['epoch'], df_val['val_loss'], color='green', linewidth=0.8, label='Val Loss')
+
+    ax1.set_xscale('log')       
+    ax1.set_xlabel('Epoch (Log Scale)')
+    ax1.set_ylabel('Loss')
+
+    ax2 = ax1.twinx()
+    ax2.plot(df_ph['epoch'], df_ph['phdim_0'], color='red', linewidth=0.8, label='Persistent Homology Dimension')
+    
+    #ax2.set_yscale('log')
+    #ax2.set_ylim(bottom=0)
+    ax2.set_ylabel('Persistent Homology Dimension')
+    ax2.tick_params(axis='y')
+
+    plt.title(f"{input_dir} operation")
+
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left', fontsize='x-small')
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(input_parent_dir, input_dir + "_loss.png"))
+    plt.show()
+
+def plot_graphs(operation):
+    plot_losses_phdim_against_epochs(operation)
+    plot_accuracies_phdim_against_epochs(operation)
+
+def concat_csvs(csv_list, output_dir):
+    
     df_list = [pd.read_csv(f) for f in csv_list]
     combined_df = pd.concat(df_list, ignore_index=True)
-    combined_df.to_csv(output_name, index=False)
+
+    root = "fyp/data"
+    full_output_dir = os.path.join(root, output_dir)
+    os.makedirs(full_output_dir, exist_ok=True)
+    csv_path = os.path.join(full_output_dir, output_dir + ".csv")
+    combined_df.to_csv(csv_path, index=False)
 
     for f in csv_list:
         try:
@@ -51,7 +110,9 @@ def concat_csvs(csv_list, output_name):
         except OSError as e:
             print(f"Error deleting {f}: {e}")
 
-    print(f"Combined {len(csv_list)} files into {output_name}")
+    print(f"Combined {len(csv_list)} files into {csv_path}")
 
 if __name__ == "__main__":
-    plot_accuracies_phdim_against_epochs("fyp/addition.csv", "fyp/addition.png")
+    #concat_csvs([f"fyp/data/multiplication/metrics ({i}).csv" for i in range(1,4)], "multiplication")
+    plot_graphs("addition")
+    plot_graphs("multiplication")
